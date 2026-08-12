06f4951c5a71321cf1c47bb7293ada586cd8d347
SJ-Alumno <alayojenniffer@gmail.com>
2026-08-12 11:34:48 -0400

    first commit

---
+++ src/main/java/dev/jenniffer/Prestamo.java
@@ -0,0 +1,29 @@
+package dev.jenniffer;
+
+public class Prestamo {
+    String fechaPrestamo;
+    int montoPrestamo;
+    float tasaPrestamo;
+    int coutas;
+
+    public Prestamo(){
+
+    }
+    public Prestamo(String fechaPrestamo, int montoPrestamo, float tasaPrestamo, int coutas){
+    }
+}
+    public boolean solicitar(){
+    return true;
+    }
+
+
+    public int pagarCuota(int cuota){
+    return 0;
+
+
+
+
+
+
+
+}
